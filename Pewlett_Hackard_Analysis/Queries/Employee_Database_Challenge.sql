20cdc5e8eb14ca4f093e4c9ed983627dfaea9f01
Adam Karim <akarim13@aol.com>
2022-09-06 15:30:38 -0400

    DELIVERABLE 2

--- Pewlett_Hackard_Analysis/Queries/Employee_Database_Challenge.sql
+++ Pewlett_Hackard_Analysis/Queries/Employee_Database_Challenge.sql
@@ -16,7 +16,7 @@ SELECT DISTINCT ON (re.emp_no) re.emp_no,
 re.first_name,
 re.last_name,
 re.title
-INTO unique_title
+INTO unique_titles
 FROM retirement_titles AS re
 WHERE (re.to_date = '9999-01-01')
 ORDER BY re.emp_no, re.to_date DESC;
@@ -27,3 +27,17 @@ INTO retiring_titles
 FROM unique_title as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+-- Deliverable 2
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no , e.first_name, e.last_name , e.birth_date,
+	de.from_date, de.to_date,
+	ti.title
+INTO mentorship_eligibility
+FROM employees as e
+INNER JOIN dept_employee as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN title as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no ;
